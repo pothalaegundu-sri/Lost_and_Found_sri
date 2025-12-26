2ea48f576b7704d5a31bd61cfb5449021f54e21e
pothalaegundu-sri <pothalaegundu@gmail.com>
2025-12-26 07:15:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -296,8 +296,10 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+# 👇 NO SPACES at the start of this line!
+with app.app_context():
+    # Creates database tables if they don't exist
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        # Creates database tables if they don't exist
-        db.create_all()
     app.run(debug=True)
